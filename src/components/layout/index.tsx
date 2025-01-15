b96d948e98058492ea0c940e8deb25acdbaac1a0
= <=>
2025-01-15 16:09:10 +0530

    Add Loader Animation and custom cursor

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,18 @@
 import Header from "./header";
 import Footer from "./footer";
 import ScrollToTop from "./scroll-to-top";
+import LoaderAnimation from "./loader";
+import CustomCursor from "../custom-cursor";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
+			<LoaderAnimation />
 			<Header />
 			<main>{children}</main>
 			<Footer />
 			<ScrollToTop />
+			<CustomCursor />
 		</>
 	);
 }
